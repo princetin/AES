1dd87b2386980c7c00f5574d1b3b5f8bb82b16c0
User <ilin.al.03@mail.ru>
2026-07-16 19:16:53 +0300

    .

--- AES.py
+++ AES.py
@@ -46,13 +46,13 @@ def aes(message: str, p, g):
     for elements_in_blocks in blocks:
         State_matrix = np.array(elements_in_blocks).reshape(4, 4).T
         RK_xor_matrix = hex_xor_matrices(State_matrix, short_key_matrix)
-        #print(RK_xor)
+        #print(RK_xor_matrix)
         RK_xor_matrix = np.array([[int(x, 16) for x in row] for row in RK_xor_matrix])
 
         vectorized_sbox = np.vectorize(replacement_S_box)
         result_numbers = vectorized_sbox(RK_xor_matrix)
         SubBytes_matrix = np.array([[f'{x:02x}' for x in row] for row in result_numbers])
-        #print(SubBytes)
+        #print(SubBytes_matrix)
 
         Shifted_roll_matrix = np.array([
             SubBytes_matrix[0],
@@ -61,6 +61,8 @@ def aes(message: str, p, g):
             np.roll(SubBytes_matrix[3], -3)
         ])
         print(Shifted_roll_matrix)
+
+
 
 
 RFC_3526 = 32317006071311007300338913926423828248817941241140239112842009751400741706634354222619689417363569347117901737909704191754605873209195028853758986185622153212175412514901774520270235796078236248884246189477587641105928646099411723245426622522193230540919037680524235519125679715870117001058055877651038861847280257976054903569732561526167081339361799541336476559160368317896729073178384589680639671900977202194168647225871031411336429319536193471636533209717077448227988588565369208645296636077250268955505928362751121174096972998068410554359584866583291642136218231078990999448652468262416972035911852507045361090559
